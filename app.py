d11da75115d2f561ce6c7b7be13f50a0cd3149c7
genuine-avi <75043113+genuine-avi@users.noreply.github.com>
2023-06-10 20:28:20 +0000

    Added Dynamic Variables

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import Flask,render_template
 app=Flask(__name__)
+JOBS=[
+  {
+    'id':1,
+    'title':'Data-Analyst',
+    'location':'Bengaluru,India',
+    'salary':'Rs 1,000,000',
+  },
+{
+    'id':2,
+    'title':'Data-Scientist',
+    'location':'Delhi,India',
+    'salary':'Rs 1,060,000',
+  },
+{
+    'id':3,
+    'title':'Front-End Engineer',
+    'location':'Gurgaon,India',
+    'salary':'Rs 15,000,000',
+  },
+{
+    'id':4,
+    'title':'Data-Analyst',
+    'location':'Remote',
+    'salary':'$ 1,000,0000',
+  },
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS,company_name='Aviral')
 
 if __name__=='__main__':
  app.run(host='0.0.0.0',debug=True)
