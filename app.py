6173e6265fe41e9e9c25f31838f63393148ff83c
genuine-avi <75043113+genuine-avi@users.noreply.github.com>
2023-06-11 18:44:21 +0000

    Added Dynamic Data and API Route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 app=Flask(__name__)
 JOBS=[
   {
@@ -29,6 +29,10 @@ JOBS=[
 @app.route("/")
 def hello_world():
   return render_template('home.html',jobs=JOBS,company_name='Aviral')
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 
 if __name__=='__main__':
  app.run(host='0.0.0.0',debug=True)
