5d3430fb531064fe1840f0df1652a64c71cbcb89
genuine-avi <75043113+genuine-avi@users.noreply.github.com>
2023-06-24 21:24:47 +0000

    Load Job From DB

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
 from flask import Flask,render_template,jsonify
+from database import load_jobs_from_db
 app=Flask(__name__)
-JOBS=[
-  {
-    'id':1,
-    'title':'Data-Analyst',
-    'location':'Bengaluru,India',
-    'salary':'Rs 1,000,000',
-  },
-{
-    'id':2,
-    'title':'Data-Scientist',
-    'location':'Delhi,India',
-    'salary':'Rs 1,060,000',
-  },
-{
-    'id':3,
-    'title':'Front-End Engineer',
-    'location':'Gurgaon,India',
-    'salary':'Rs 15,000,000',
-  },
-{
-    'id':4,
-    'title':'Data-Analyst',
-    'location':'Remote',
-    'salary':'$ 1,000,0000',
-  },
-]
+# JOBS=[
+#   {
+#     'id':1,
+#     'title':'Data-Analyst',
+#     'location':'Bengaluru,India',
+#     'salary':'Rs 1,000,000',
+#   },
+# {
+#     'id':2,
+#     'title':'Data-Scientist',
+#     'location':'Delhi,India',
+#     'salary':'Rs 1,060,000',
+#   },
+# {
+#     'id':3,
+#     'title':'Front-End Engineer',
+#     'location':'Gurgaon,India',
+#     'salary':'Rs 15,000,000',
+#   },
+# {
+#     'id':4,
+#     'title':'Data-Analyst',
+#     'location':'Remote',
+#     'salary':'$ 1,000,0000',
+#   },
+# ]
+
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html',jobs=JOBS,company_name='Avirals')
+  jobs=load_jobs_from_db()
+  return render_template('home.html',jobs=jobs,company_name='Avirals')
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs=load_jobs_from_db()
+  return jsonify(jobs)
   
 
 if __name__=='__main__':
